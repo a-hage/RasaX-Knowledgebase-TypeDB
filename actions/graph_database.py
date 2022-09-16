15d87acc83775563bef7e8c9ddb5529ec1b58a3e
Abdo El Hage <47995042+a-hage@users.noreply.github.com>
2022-09-16 16:17:27 +0200

    Update graph_database.py
    change the host

--- actions/graph_database.py
+++ actions/graph_database.py
@@ -35,7 +35,7 @@ class GraphDatabase(KnowledgeBase):
     sure to have the graph database set up and the TyprDB server running.
     """
 
-    def __init__(self, uri: Text = "http://10.210.1.151:1729", keyspace: Text = "banking"):
+    def __init__(self, uri: Text = "http://127.0.0.1:1729", keyspace: Text = "banking"):
         self.uri = uri
         self.keyspace = keyspace
         self.me = "mitchell.gillis@t-online.de"
